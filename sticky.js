d6358e667c4e0a74eeb55df65b2ec1e1a8f42351
Daniel <danieljkahn@gmail.com>
2021-04-28 18:48:47 -0500

    cleaned up a bit of variable declarations, moving everything to the constructor

--- sticky.js
+++ sticky.js
@@ -4,6 +4,10 @@ class Sticky {
 		this.stickyEl = stickyEl;
 		this.boundaryEl = boundaryEl;
 		this.stuckScroll = 0;
+		this.elementPos = this.stickyEl.getBoundingClientRect();
+		this.boundaryPos = this.boundaryEl.getBoundingClientRect();
+		this.elY = this.elementPos.y;
+		this.boundY = this.boundaryPos.y + this.boundaryPos.height;
 		
 		this.log = log;
 		this.scroller = scroller;
@@ -25,24 +29,25 @@ class Sticky {
 				docTop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 			}
 			self.makeSticky(docTop);
-
+			if (self.log) {
+				self.logStuff();
+			}
 		});
 	}
 	makeSticky(docTop) {
-		let elementPos = this.stickyEl.getBoundingClientRect(),
-			boundaryPos = this.boundaryEl.getBoundingClientRect(),
-			elY = elementPos.y,
-			boundY = boundaryPos.y + boundaryPos.height;
-		if ( (elY <= boundY) && !this.stickyEl.classList.contains('sticky')) {
+		this.elementPos = this.stickyEl.getBoundingClientRect();
+		this.boundaryPos = this.boundaryEl.getBoundingClientRect();
+		this.elY = this.elementPos.y;
+		this.boundY = this.boundaryPos.y + this.boundaryPos.height;
+		if ( (this.elY <= this.boundY) && !this.stickyEl.classList.contains('sticky')) {
 			this.stickyEl.classList.add('sticky');
-			this.ogPos = docTop;
-
+			this.ogPos = docTop; /// WTF
 		} else if (docTop <= this.ogPos) {
 				this.stickyEl.classList.remove('sticky');
 		}
-		if (this.log) {
-			console.log(this.stickyEl.classList[0]+': element pos: '+elY);
-			console.log('this.ogPos: '+this.ogPos);
-		}
+	}
+	logStuff() {
+		console.log(this.stickyEl.classList[0]+': element pos: '+this.elY);
+		console.log('this.ogPos: '+this.ogPos);
 	}
 }
